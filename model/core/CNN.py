f61359199756b2f42a91d9742913d50d72677dbe
Frances <zhouxian@sjtu.edu.cn>
2018-01-09 16:51:51 +0800

    fix bug

--- model/core/CNN.py
+++ model/core/CNN.py
@@ -36,7 +36,9 @@ class CNN(object):
             # hidden_states: [batch_size, num, hidden_dim]
             # label_embeddings: [batch_size, label_embedding_dim]
             # score: h*W*l
-            s = tf.matmul(tf.matmul(hidden_states, w), tf.expand_dims(label_embeddings, axis=-1))
+            #s = tf.matmul(tf.matmul(hidden_states, w), tf.expand_dims(label_embeddings, axis=-1))
+            s = tf.matmul(tf.reshape(tf.matmul(tf.reshape(hidden_states, [-1, hidden_dim]), w), [self.batch_size, -1, hidden_dim]),
+                          tf.expand_dims(label_embeddings, axis=-1))
             # s: [batch_size, num, 1]
             s = tf.nn.softmax(s, 1)
             # s: [batch_size, num, 1]
