3f2dbd606070a61be1dddcf55938e26a9a1c4d5f
Frances <zhouxian@sjtu.edu.cn>
2018-01-12 14:02:16 +0800

    add max_seq_len

--- train_wiki10_31k.py
+++ train_wiki10_31k.py
@@ -34,6 +34,8 @@ def main():
                        default='datasets/material/word_embeddings.npy',
                        help='path to the word embeddings')
     # ---------- model ----------
+    parse.add_argument('-max_seq_len', '--max_seq_len', type=int, default=500,
+                       help='maximum sequence length')
     parse.add_argument('-model', '--model', type=str, default='biLSTM', help='model: LSTM, biLSTM, CNN')
     parse.add_argument('-pretrained_model', '--pretrained_model_path', type=str,
                        default=None, help='path to the pretrained model')
@@ -73,7 +75,7 @@ def main():
     test_candidate_label = load_pickle(args.folder_path + 'test_candidate_label.pkl')
     print 'number of labels: ' + str(len(all_labels))
     print 'create train/test data loader...'
-    train_loader = DataLoader4(train_doc, train_label, train_candidate_label, all_labels, label_embeddings, args.batch_size, vocab, word_embeddings, given_seq_len=False, max_seq_len=5000)
+    train_loader = DataLoader4(train_doc, train_label, train_candidate_label, all_labels, label_embeddings, args.batch_size, vocab, word_embeddings, given_seq_len=False, max_seq_len=args.max_seq_len)
     max_seq_len = train_loader.max_seq_len
     print 'max_seq_len: ' + str(max_seq_len)
     test_loader = DataLoader4(test_doc, test_label, test_candidate_label, all_labels, label_embeddings, args.batch_size, vocab, word_embeddings, given_seq_len=True, max_seq_len=max_seq_len)
