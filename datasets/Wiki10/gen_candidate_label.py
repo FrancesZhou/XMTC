bfee01b4c7caaddf8504714cc5cc30042e6c7248
Frances <zhouxian@sjtu.edu.cn>
2018-01-30 21:22:43 +0800

    fix

--- datasets/Wiki10/gen_candidate_label.py
+++ datasets/Wiki10/gen_candidate_label.py
@@ -55,7 +55,7 @@ def get_candidate_labels(path, out_path, type, format):
             for l_s in candidate_label_line.split(' '):
                 l_, s_ = l_s.split(':')
                 ll = index_label[int(l_)]
-                temp.append((ll, s_))
+                temp.append((ll, float(s_)))
             sorted_temp = sorted(temp, key=lambda e: e[1], reverse=True)
             train_candidate_labels[pid] = dict(sorted_temp[:30])
             # candidate_label_score = {}
@@ -71,7 +71,7 @@ def get_candidate_labels(path, out_path, type, format):
             for l_s in candidate_label_line.split(' '):
                 l_, s_ = l_s.split(':')
                 ll = index_label[int(l_)]
-                temp.append((ll, s_))
+                temp.append((ll, float(s_)))
             sorted_temp = sorted(temp, key=lambda e: e[1], reverse=True)
             test_candidate_labels[pid] = dict(sorted_temp[:30])
             # candidate_label_score = {}
