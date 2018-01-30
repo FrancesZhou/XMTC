3b8877eac5bc0eb9c6dfbc56f2b1e8c5e5b1dcfb
Frances <zhouxian@sjtu.edu.cn>
2018-01-30 16:48:28 +0800

    fix

--- datasets/Amazon-670K/gen_candidate_label.py
+++ datasets/Amazon-670K/gen_candidate_label.py
@@ -6,46 +6,84 @@ Created on Dec, 2017
 
 from __future__ import absolute_import
 
-#import argparse
+import argparse
 import numpy as np
 import sys
 import scipy.io as sio
 sys.path.append('../material')
 from utils import load_pickle, dump_pickle, load_txt
 
-path = 'data/baseline_data/adjacent_labels/'
-out_path = 'data/deeplearning_data/adjacent_labels/'
 
-train_asins_file = path + 'train_map.txt'
-test_asins_file = path + 'test_map.txt'
-train_candidate_label_file = path + 'candidate_train.mat'
-test_candidate_label_file = path + 'candidate_test.mat'
-label_index_file = path + 'label_dict.pkl'
-
-
-def get_candidate_labels():
-    train_asins = load_txt(train_asins_file)
-    test_asins = load_txt(test_asins_file)
-    train_candidate_all = sio.loadmat(train_candidate_label_file)['candidate_train']
-    test_candidate_all = sio.loadmat(test_candidate_label_file)['candidate_test']
-    label_index = load_pickle(label_index_file)
-    index_label = dict(zip(label_index.values(), label_index.keys()))
+def get_candidate_labels(path, out_path, type, format):
+    train_titles_file = path + 'train_map.txt'
+    test_titles_file = path + 'test_map.txt'
+    # train_candidate_label_file = path + type + '_candidate/candidate_train.mat'
+    # test_candidate_label_file = path + type + '_candidate/candidate_test.mat'
+    #label_index_file = path + 'label_dict.pkl'
+    index_label_file = path + 'all_labels.pkl'
+    train_titles = load_txt(train_titles_file)
+    test_titles = load_txt(test_titles_file)
+    index_label = load_pickle(index_label_file)
     train_candidate_labels = {}
     test_candidate_labels = {}
-    # train data
-    for i in range(len(train_asins)):
-        pid = train_asins[i].strip()
-        pid = int(pid)
-        candidate_label_index = train_candidate_all[i]
-        candidate_labels = [index_label[ind] for ind in candidate_label_index]
-        train_candidate_labels[pid] = candidate_labels
-    for i in range(len(test_asins)):
-        pid = test_asins[i].strip()
-        pid = int(pid)
-        candidate_label_index = test_candidate_all[i]
-        candidate_labels = [index_label[ind] for ind in candidate_label_index]
-        test_candidate_labels[pid] = candidate_labels
-    dump_pickle(train_candidate_labels, out_path + 'train_candidate_label.pkl')
-    dump_pickle(test_candidate_labels, out_path + 'test_candidate_label.pkl')
+    if format == 'mat':
+        train_candidate_label_file = path + type + '_candidate/candidate_train.mat'
+        test_candidate_label_file = path + type + '_candidate/candidate_test.mat'
+        train_candidate_all = sio.loadmat(train_candidate_label_file)['candidate_train']
+        test_candidate_all = sio.loadmat(test_candidate_label_file)['candidate_test']
+        for i in xrange(len(train_titles)):
+            pid = train_titles[i].strip()
+            pid = int(pid)
+            candidate_label_index = train_candidate_all[i]
+            candidate_labels = [index_label[ind] for ind in candidate_label_index]
+            train_candidate_labels[pid] = candidate_labels
+        for i in xrange(len(test_titles)):
+            pid = test_titles[i].strip()
+            pid = int(pid)
+            candidate_label_index = test_candidate_all[i]
+            candidate_labels = [index_label[ind] for ind in candidate_label_index]
+            test_candidate_labels[pid] = candidate_labels
+    elif format == 'txt':
+        train_candidate_label_file = path + type + '_candidate/train_score_mat.txt'
+        test_candidate_label_file = path + type + '_candidate/test_score_mat.txt'
+        train_candidate_all = load_txt(train_candidate_label_file)[1:]
+        test_candidate_all = load_txt(test_candidate_label_file)[1:]
+        for i in xrange(len(train_titles)):
+            pid = int(train_titles[i].strip())
+            candidate_label_line = train_candidate_all[i].strip()
+            candidate_label_score = {}
+            for l_s in candidate_label_line.split(' ')[:50]:
+                l_, s_ = l_s.split(':')
+                ll = index_label[int(l_)]
+                candidate_label_score[ll] = float(s_)
+            train_candidate_labels[pid] = candidate_label_score
+        for i in xrange(len(test_titles)):
+            pid = int(test_titles[i].strip())
+            candidate_label_line = test_candidate_all[i].strip()
+            candidate_label_score = {}
+            for l_s in candidate_label_line.split(' ')[:50]:
+                l_, s_ = l_s.split(':')
+                ll = index_label[int(l_)]
+                candidate_label_score[ll] = float(s_)
+            test_candidate_labels[pid] = candidate_label_score
 
-get_candidate_labels()
+    dump_pickle(train_candidate_labels, out_path + type + '_candidate/train_candidate_label.pkl')
+    dump_pickle(test_candidate_labels, out_path + type + '_candidate/test_candidate_label.pkl')
+
+def main():
+    parse = argparse.ArgumentParser()
+    # ---------- environment setting: which gpu -------
+    parse.add_argument('-data_format', '--data_format', type=str,
+                       default='txt', help='mat file or txt file (which way to store candidate labels)')
+    parse.add_argument('-can_type', '--candidate_type', type=str,
+                       default='sleec', help='sleec or fastxml (which method to generate candidate labels)')
+
+    args = parse.parse_args()
+
+    path = 'data/baseline_data/adjacent_labels/'
+    out_path = 'data/deeplearning_data/adjacent_labels/'
+
+    get_candidate_labels(path, out_path, args.candidate_type, args.data_format)
+
+if __name__ == "__main__":
+    main()
--- datasets/AmazonCat-13K/inv_propensity.py
+++ datasets/AmazonCat-13K/inv_propensity.py
@@ -30,21 +30,30 @@ def main():
     args = parse.parse_args()
 
     data_path = 'data/deeplearning_data/adjacent_labels/'
-    train_title_label = load_pickle(data_path + 'train_asin_label.pkl')
+    train_title_label = load_pickle(data_path + 'train_label.pkl')
+
+    index_label = load_pickle('data/baseline_data/adjacent_labels/all_labels.pkl')
+    baseline_inv_prop_file = 'data/baseline_data/adjacent_labels/inv_prop.txt'
 
     train_label = train_title_label.values()
     train_label = np.concatenate(train_label).tolist()
     label_frequency = dict(Counter(train_label))
-    labels = label_frequency.keys()
-    fre = np.array(label_frequency.values())
+    labels, fre = zip(*label_frequency.iteritems())
+    fre = np.array(fre)
 
     N = len(train_title_label)
     C = (math.log(N)-1) * (args.B + 1)**args.A
     inv_prop = 1 + C * (fre + args.B)**(-args.A)
 
     inv_prop_dict = dict(zip(labels, inv_prop.tolist()))
-    dump_pickle(inv_prop_dict, data_path + 'inv_prop.pkl')
-
+    dump_pickle(inv_prop_dict, data_path + 'inv_prop_dict.pkl')
+    #
+    # for baseline inv propensity
+    with open(baseline_inv_prop_file, 'w') as df:
+        for l_ in index_label[:-1]:
+            df.write(str(inv_prop_dict[l_]))
+            df.write('\n')
+        df.write(str(inv_prop_dict[index_label[-1]]))
 
 
 if __name__ == "__main__":
