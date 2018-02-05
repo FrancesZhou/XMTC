548cf1e34f4fbd13c405ce0bb717c344d886d39a
Frances <zhouxian@sjtu.edu.cn>
2018-02-05 14:28:19 +0800

    initialize label embedding

--- model/core/CNN.py
+++ model/core/CNN.py
@@ -24,8 +24,8 @@ class CNN(object):
         self.const_initializer = tf.constant_initializer()
         #
         self.word_embedding = tf.constant(word_embedding, dtype=tf.float32)
-        self.label_embedding = tf.constant(label_embedding, dtype=tf.float32)
-        self.label_embedding = tf.get_variable('label_embedding', [], initializer=label_embedding)
+        #self.label_embedding = tf.constant(label_embedding, dtype=tf.float32)
+        self.label_embedding = tf.get_variable('label_embedding', label_embedding.shape, initializer=label_embedding)
         #
         # self.x = tf.placeholder(tf.float32, [self.batch_size, self.max_seq_len, self.word_embedding_dim])
         # self.seqlen = tf.placeholder(tf.int32, [self.batch_size])
